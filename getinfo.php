a92bf9d3d206f06b27f2d5fe07ebad1ca477bc5f
Yudhi <yudhi@yudhi-PC.(none)>
2013-06-18 09:21:10 +0700

    add getinfo.php test.html

---
+++ getinfo.php
@@ -0,0 +1,11 @@
+<?php
+require_once("data.php");
+
+$id = $_GET['id'];
+$data = new baseObj();
+
+if (is_object($data) == true) $status = '200 OK';
+$status_header = 'HTTP/1.1 '.$status;
+
+header($status_header);
+echo json_encode( $data->getAll($id) );
